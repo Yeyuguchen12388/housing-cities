6a5883867c21ca484dc0d1a541995a7c44f6847f
Yeyuguchen12388 <148785111+Yeyuguchen12388@users.noreply.github.com>
2024-09-24 14:22:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-from modules.data_loader import load_data
-from modules.filters import apply_filters
-from modules.visualizations import plot_map, plot_histogram
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # 应用标题
 st.title("California Housing Data (1990)")
 
 # 加载数据
-data = load_data("data/housing.csv")
+data = pd.read_csv("./housing.csv")
 
 # 显示原始数据
 st.write("Housing Data Overview", data.head())
@@ -16,13 +15,13 @@ st.write("Housing Data Overview", data.head())
 min_price = st.slider("Select Minimum Median House Price", 0, 500001, 200000)
 
 # 过滤数据
-filtered_data = apply_filters(data, min_price)
 
 # 显示地图
-plot_map(filtered_data)
+st.map(filtered_data)
 
 # 显示直方图
-plot_histogram(filtered_data)
+plt.hist(filtered_data)
+st.pyplot(plt)
 
 # 显示最终过滤后的数据
 st.write("Filtered Housing Data", filtered_data)
